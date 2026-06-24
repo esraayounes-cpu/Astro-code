17b01ea99806e45f9360202179a883cad2b4dfa3
Maria <mariaatef377@gmail.com>
2026-06-25 00:37:49 +0300

    done

--- galaxy_sed/__init__.py
+++ galaxy_sed/__init__.py
@@ -2,7 +2,7 @@ import os
 
 __version__ = "0.0.1"
 
-from .galaxy_sed.loadData_sdss import load_sdss
+from .loadData_sdss import load_sdss
 from .sed_filters import SDSS_FILTERS
 from .sed_plot import plot_sed
 from .data import convert_sdss_photometry, load_sdss_fits
